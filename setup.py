07717587c83625e804828a761197942a27017582
Andrew Otto <acotto@gmail.com>
2014-04-02 18:17:36 -0400

    Version 0.0.7
    Change-Id: I42d22d5f8ad5986a8c5a8ff84fe8f98297bb4b55

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='logster',
-    version='0.0.6',
+    version='0.0.7',
     description='Parse log files, generate metrics for Graphite and Ganglia',
     author='Etsy',
     url='https://github.com/etsy/logster',
