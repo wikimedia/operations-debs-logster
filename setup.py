086bf1c72b2737bc6934e06b34ede42d839cac06
Andrew Otto <acotto@gmail.com>
2014-12-03 17:34:23 -0500

    Bump version to 0.8
    Change-Id: I7bc1d1e5abdcb0187490ee173ebada09a6f0f5a8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='logster',
-    version='0.0.7',
+    version='0.0.8',
     description='Parse log files, generate metrics for Graphite and Ganglia',
     author='Etsy',
     url='https://github.com/etsy/logster',
